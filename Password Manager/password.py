e16e062bf3bedee09351f450fa1bce57575db9ed
Mawande John PAul <johnpauljones181103psn@gmail.com>
2024-10-17 15:25:08 -0700

    latest changes

--- Password Manager/password.py
+++ Password Manager/password.py
@@ -1,94 +1,36 @@
-def newuser():
-    username = input('Enter username : ')
-
-    pwd1 = input('Enter password : ')
-    pwd2 = input('Enter password again : ')
-
-    while pwd1 != pwd2:
-        pwd1 = input('Enter password : ')
-        pwd2 = input('Enter password again : ')
-
-    print (f'Hello {username}, your password is {pwd1}, please note it as it wont be visible again!')
-
-    with open('Users.file', 'a') as file:
-        file.write(username + ' ' + pwd1 + '\n')
+def save_user(username, password):
+    users[username] = password
     
-    userfile = username  + '.file'
-    with open(userfile, 'w') as file:
-        file.close()
 
-    openuser()
-
-def openuser():
-    users = []
-    usernames = []
-    passwords = []
-    with open('Users.file', 'r') as file:
-        users = file.readlines()
-        for user in users:
-            user.strip()
-            userswithpasswords = user.split(' ')
-            usernames.append(userswithpasswords[0])
-            passwords.append(userswithpasswords[1])
-
-    index = 0
-    indexes = []
-
-    print('Enter the number corresponding to your username;')
-    for username in usernames:
-        print(f'{index} : {username}\n')
-        indexes.append(index)
-        index += 1
-    choice = int(input('You are user number : '))
-
-    for index in indexes:
-        if choice == index:
-            with open('Users.file', 'r') as file:
-                users = [user.strip() for user in file.readlines()]
-                usernamewithpassword = users[index]
-                wordlist = usernamewithpassword.split(' ')
-                username = wordlist[0] + '.file'
-                password = wordlist[1]
-
-            entry = input('Enter password : ')
-            if entry == password:
-                with open(username, 'r') as file:
-                    pwds = [pwd.strip() for pwd in file.readlines()]
-                    for pwd in pwds:
-                        print(pwd + '\n')
-
-                choice = input('For new password press N, to quit press Q : ')
-                if choice == 'N' or choice == 'n':
-                    pwdname = input('What is this password for : ')
-                    pwdnew = input('What is the password : ')
-
-                    with open(username + '.file', 'w') as file:
-                        file.write(pwdname + pwdnew + '\n')
-                        pwds = [pwd.strip() for pwd in file.readlines()]
-                        for pwd in pwds:
-                            print(pwd + '\n')
-                
-                else:
-                    quit()
-
-            else:
-                print('Wrong password!!')
+def new_user():
+    username = input('Choose a username: ')
+    password = input('Choose a password: ')
+    while True:
+        password_repeat = input('Repeat the password: ')
+        if password_repeat == password:
             break
-        else:
-            print('User not found')
+        print('Passwords dont match !!')
+    save_user(username, password)
+    open_user(username, password)
+def open_user(username, password):
+    pass
 
-
-def start():
-    choice = input('New user? Enter Y/N :')
-    if choice == 'Y' or choice == 'y':
-        newuser()
-
-    elif choice == 'N' or choice == 'n':
-        openuser()
-
-    else:
-        print('Invalid input')
+try:
+    with open('users.json', 'x') as file:
+        file.close()
+except:
+    pass
 
 while True:
-    start()
+    print('New user? Y/N: ')
+    print('Press Q to quit: ')
 
+    choice = input('Enter choice: ')
+    if choice.upper() == 'Y':
+        new_user()
+    elif choice.upper() == 'N':
+        open_user()
+    elif choice.upper() == 'Q':
+        quit()
+    else:
+        print('Invalid Input!!')
