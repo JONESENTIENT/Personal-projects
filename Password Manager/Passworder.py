4f6f59cda5a4e6c09f12445f40143eb3b467026c
Mawande John PAul <johnpauljones181103psn@gmail.com>
2024-10-28 15:06:28 -0700

    added options for characters and alphanumerics

--- Password Manager/Passworder.py
+++ Password Manager/Passworder.py
@@ -5,12 +5,29 @@ def generate():
     letters = r'abcdefghijklmopqrstuvwxyz 0123456789#.,@\n/?-_'
 
     word = ''
-    for j in range(10):
-        number = random.randint(0,45)
-        letter = letters[number]
-        word += letter
+    num = choice3.get()
+    space = choice1.cget()
+    alpha = choice2.cget()
+    print(space, alpha)
+    if num >= 4 and num <= 10:
+        for j in range(num):
+            number = random.randint(0,45)
+            letter = letters[number]
+            if space == 0:
+                if letter in r' ':
+                    continue
+            elif alpha == 0:
+                if letter in r'#.,@\n/?-_':
+                    continue
+            else:
+                word += letter
 
-    label.configure(text=word)
+        label.configure(text=word)
+    elif num < 4 or num > 10:
+        label.configure(text = 'Too few/many characters!')
+
+    else:
+        label.configure(text = 'Invalid password length!')
 
 root = ctk.CTk()
 root.geometry('500x130')
@@ -27,15 +44,15 @@ button = ctk.CTkButton(root, width=100, height=45, text='Generate', command=gene
 button.place(x=380,y=10)
 
 choice1 = ctk.CTkRadioButton(root, width=125, height=50, text='Include spaces?')
-choice1.place(x=150, y=70)
+choice1.place(x=180, y=70)
 
 choice2 = ctk.CTkRadioButton(root, width=125, height=50, text='Include alphanumerics?')
-choice2.place(x=300, y=70)
+choice2.place(x=330, y=70)
 
 choice3 = ctk.CTkEntry(root, width=50, height=50)
 choice3.place(x=10, y=70)
 
-label = ctk.CTkLabel(root, width=90, height=50, text='Characters')
-label.place(x=60,y=70)
+label2 = ctk.CTkLabel(root, width=90, height=50, text='Characters (4-10)')
+label2.place(x=60,y=70)
 
 root.mainloop()
